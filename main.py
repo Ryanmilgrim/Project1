2b431b5ae15f05a9546e976c32c018f968e3285d
Ryanmilgrim <75279313+Ryanmilgrim@users.noreply.github.com>
2023-09-03 14:34:34 -0400

    Small updatet
    changed variable name

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from datetime import date
 from Project1.data import DataCollector
 
 
-df = pd.read_csv('Project1/ticker_data.csv', index_col=0)
-data = DataCollector(df.index, date(2020, 1, 1), date(2023, 1, 1))
+tickers = pd.read_csv('Project1/ticker_data.csv').Ticker
+data = DataCollector(tickers, date(2020, 1, 1), date(2023, 1, 1))
 
 
 class BmOptimizer:
